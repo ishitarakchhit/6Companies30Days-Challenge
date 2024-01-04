738c2dff58a9d4f6025eb92186052ff1a03fad9a
Ishita Rakchhit <104856237+ishitarakchhit@users.noreply.github.com>
2024-01-04 22:23:32 +0530

    Update NumberFollowingAPattern.cpp

--- Day 2/NumberFollowingAPattern.cpp
+++ Day 2/NumberFollowingAPattern.cpp
@@ -1,21 +1,47 @@
-class DataStream {
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
 public:
-    int value, k;
-    int count = 0;
-    DataStream(int value, int k) {
-        this->value = value;
-        this->k = k;
-    }
-    
-    bool consec(int num) {
-        if(num == value){
-            count ++;
-            if(count >= k) return true;
-            else return false;
+    string printMinNumberForPattern(string pattern){
+        stack<char> s; string ans = "";
+
+        if(pattern[0] == 'I') ans += '1';
+        else s.push('1');
+
+        for(int i=1; i<=pattern.size(); i++){
+            if(pattern[i-1] == 'I'){
+                while(!s.empty()) {
+                    ans += s.top(); s.pop();
+                }
+            }
+            s.push('0' + i+ 1);
         }
-        else{
-            count = 0;
-            return false;
+
+        while(!s.empty()) {
+            ans += s.top(); s.pop();
         }
+        return ans;
     }
 };
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.printMinNumberForPattern(S) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
