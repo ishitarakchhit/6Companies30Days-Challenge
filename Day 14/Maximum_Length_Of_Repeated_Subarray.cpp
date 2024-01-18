86d8e38bd452c53503b85ca3ac108f9e25d4ee07
Ishita Rakchhit <104856237+ishitarakchhit@users.noreply.github.com>
2024-01-19 01:25:30 +0530

    Create Maximum_Length_Of_Repeated_Subarray.cpp

---
+++ Day 14/Maximum_Length_Of_Repeated_Subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        vector<vector<int>> dp(nums1.size()+1, vector<int> (nums2.size()+1, 0));
+
+        //when control reaches less than 0 for both arrays => this is represented in -1+1 = 0
+        for(int i=1; i<=nums1.size(); i++){
+            for(int j=1; j<=nums2.size(); j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+            }
+        }
+
+        //now return the largest number from the dp representing the lcs
+        int ans = 0;
+        for(auto i: dp){
+            for(auto j: i){
+                ans = max(ans, j);
+            }
+        }
+        return ans;
+    }
+};
