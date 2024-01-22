48bcac642f380d2f8820ad61c7765107ab927a5f
Ishita Rakchhit <104856237+ishitarakchhit@users.noreply.github.com>
2024-01-23 00:11:45 +0530

    Create Construct_The_Longest_New_String.cpp

---
+++ Day 16/Construct_The_Longest_New_String.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    int helper(int x, int y, int z, int last, auto &dp){
+        if(x == 0 && y == 0 && z== 0) return 0;
+
+        if(dp[x][y][z][last] != 0) return dp[x][y][z][last];
+        //add x
+        int a =0, b = 0, c= 0; 
+        if(last!=1){
+            if(x)
+                a = 2 + helper(x-1, y, z, 1, dp);
+        }
+
+        if(last!=1){
+            if(z)
+                c = 2 + helper(x, y, z-1, 3, dp);
+        }
+        //add y
+        if(last <= 1){
+            if(y)
+                b = 2+ helper(x, y-1, z, 2, dp);
+        }
+
+        dp[x][y][z][last] = max(max(a, b), c);
+        return max(max(a, b), c);
+    }
+
+    int longestString(int x, int y, int z) {
+        int last = 0;
+        int dp[51][51][51][4] = {};
+        return helper(x, y, z, last, dp);
+    }
+};
